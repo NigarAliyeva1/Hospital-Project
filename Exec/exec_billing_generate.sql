d4cd1cab203f868cc3efce48ca1ca59ddad7a28f
Nigarkhanim Aliyeva <112957859+NigarAliyeva1@users.noreply.github.com>
2024-03-02 18:29:02 +0400

    Add files via upload

---
+++ Exec/exec_billing_generate.sql
@@ -0,0 +1,289 @@
+set serveroutput on
+-- Example 1 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(145, 200);
+END;
+/
+
+
+-- Example 3 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(63, 600);
+END;
+/
+
+-- Example 4 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(101, 800);
+END;
+/
+
+-- Example 5 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(87, 1200);
+END;
+/
+
+-- Example 6 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(33, 250);
+END;
+/
+
+-- Example 7 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(179, 1500);
+END;
+/
+
+-- Example 8 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(55, 350);
+END;
+/
+
+-- Example 9 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(67, 600);
+END;
+/
+
+
+-- Example 11 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(41, 950);
+END;
+/
+
+-- Example 12 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(92, 1300);
+END;
+/
+
+-- Continue with 38 more examples, each with a unique appointment ID and a random total amount
+-- ...
+
+-- Example 13 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(178, 2200);
+END;
+/
+
+-- Example 14 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(7, 1000);
+END;
+/
+
+-- Example 15 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(56, 1600);
+END;
+/
+
+-- Example 16 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(89, 800);
+END;
+/
+
+-- Example 17 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(99, 3000);
+END;
+/
+
+-- Example 18 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(120, 750);
+END;
+/
+
+-- Example 19 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(73, 4200);
+END;
+/
+
+-- Example 20 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(16, 1100);
+END;
+/
+
+-- Example 21 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(88, 1400);
+END;
+/
+
+-- Example 22 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(45, 1900);
+END;
+/
+
+-- Example 23 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(19, 2900);
+END;
+/
+
+-- Example 24 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(29, 900);
+END;
+/
+
+-- Example 25 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(12, 6000);
+END;
+/
+
+-- Example 26 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(177, 3200);
+END;
+/
+
+-- Example 27 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(38, 1350);
+END;
+/
+
+-- Example 28 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(80, 2500);
+END;
+/
+
+-- Example 29 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(72, 2800);
+END;
+/
+
+
+-- Example 31 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(4, 3900);
+END;
+/
+
+-- Example 32 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(60, 850);
+END;
+/
+
+-- Example 33 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(91, 5100);
+END;
+/
+
+-- Example 34 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(135, 420);
+END;
+/
+
+-- Example 35 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(15, 1800);
+END;
+/
+
+-- Example 36 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(47, 2700);
+END;
+/
+
+-- Example 37 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(109, 980);
+END;
+/
+
+-- Example 38 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(28, 3600);
+END;
+/
+
+-- Example 40 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(51, 1500);
+END;
+/
+
+-- Example 41 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(111, 550);
+END;
+/
+
+-- Example 42 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(68, 3900);
+END;
+/
+
+-- Example 43 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(124, 2900);
+END;
+/
+
+-- Example 44 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(31, 820);
+END;
+/
+
+-- Example 45 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(84, 4200);
+END;
+/
+
+-- Example 46 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(5, 750);
+END;
+/
+
+-- Example 47 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(150, 1800);
+END;
+/
+
+-- Example 48 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(42, 3600);
+END;
+/
+
+-- Example 49 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(77, 900);
+END;
+/
+
+-- Example 50 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(53, 2100);
+END;
+/
+
+
+-- Example 50 with a unique appointment ID and a random total amount
+BEGIN
+    pkg_billing.generate_bill(1, 2100);
+END;
+/
---
+++ Exec/exec_billing_pay.sql
@@ -0,0 +1,179 @@
+-- Example 1 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(1, 50);
+END;
+/
+
+-- Example 2 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(2, 175);
+END;
+/
+
+-- Example 3 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(3, 180);
+END;
+/
+
+-- Example 4 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(4, 90);
+END;
+/
+
+-- Example 5 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(5, 160);
+END;
+/
+
+-- Example 6 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(6, 70);
+END;
+/
+
+-- Example 7 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(7, 30);
+END;
+/
+
+-- Example 8 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(8, 85);
+END;
+/
+
+-- Example 9 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(9, 40);
+END;
+/
+
+-- Example 10 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(10, 95);
+END;
+/
+
+-- Example 11 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(11, 155);
+END;
+/
+
+-- Example 12 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(12, 200);
+END;
+/
+
+-- Example 13 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(13, 45);
+END;
+/
+
+-- Example 14 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(14, 75);
+END;
+/
+
+-- Example 15 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(15, 20);
+END;
+/
+
+-- Example 16 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(16, 98);
+END;
+/
+
+-- Example 17 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(17, 35);
+END;
+/
+
+-- Example 18 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(18, 60);
+END;
+/
+
+-- Example 19 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(19, 25);
+END;
+/
+
+-- Example 20 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(20, 88);
+END;
+/
+
+-- Example 21 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(21, 70);
+END;
+/
+
+-- Example 22 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(22, 45);
+END;
+/
+
+-- Example 23 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(23, 50);
+END;
+/
+
+-- Example 24 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(24, 30);
+END;
+/
+
+-- Example 25 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(25, 99);
+END;
+/
+
+-- Example 26 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(26, 22);
+END;
+/
+
+-- Example 27 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(27, 78);
+END;
+/
+
+-- Example 28 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(28, 87);
+END;
+/
+
+-- Example 29 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(29, 63);
+END;
+/
+
+-- Example 30 with a unique bill ID and a random paid amount less than 100
+BEGIN
+    pkg_billing.pay_bill(30, 92);
+END;
+/
